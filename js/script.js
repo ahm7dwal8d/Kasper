8b1968fcbceeb384c19b603f374dae599c1f8774
ahmedwal8d <85419960+ahmedwal8d@users.noreply.github.com>
2022-05-10 17:41:37 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 // Header Section 
 
-$(".header .link i").click(function () {
+
+$(".header .link > i").click(function () {
 
     $(".header .link ul").slideToggle()
 
@@ -21,6 +22,92 @@ $(".header ul li a").click(function (event) {
     })
 
 })
+
+$(window).scroll(function () {
+
+    if ($(window).scrollTop() >= 1000) {
+
+        $(".header").addClass("active")
+
+    } else {
+
+        $(".header").removeClass("active")
+
+    }
+
+})
+
+// Landing Section 
+
+let landingSlide = document.querySelectorAll(".landing .slide")
+
+let landingArrey = ["landing.jpg" , "landing-2.jpg" , "landing-3.jpg" , "landing-4.jpg"]
+
+landingSlide.forEach((slide)=> {
+
+    setInterval(function () {
+
+        let randomNumber = Math.floor(Math.random() * landingArrey.length)
+
+        slide.style.backgroundImage = "url(images/"+ landingArrey[randomNumber] +")"
+
+    } , 10000)
+})
+
+let sliderWrap = document.querySelector(".landing .slider-wrap")
+
+let slider = sliderWrap.getElementsByClassName("slide")
+
+function next() {
+
+    sliderWrap.append(slider[0])
+
+}
+
+function prev() {
+
+    sliderWrap.prepend(slider[slider.length - 1])
+
+}
+
+$(".landing .bullets li").click(function () {
+
+    $(this).addClass("active").siblings().removeClass("active")
+
+})
+
+
+
+// Box Model Section 
+
+$(".box-model .icon").click(function () {
+
+    $(".box-model").toggleClass("active")
+
+})
+
+document.body.classList.add(localStorage.getItem("bodyColor"))
+
+let elColor = document.querySelectorAll(".box-model .color ul li")
+
+let bodyColor = []
+
+
+for (let i = 0; i < elColor.length; i++) {
+
+    bodyColor.push(elColor[i].getAttribute("data-color"))
+
+    elColor[i].addEventListener("click" , function () {
+
+        document.body.classList.remove(...bodyColor)
+
+        document.body.classList.add(elColor[i].getAttribute("data-color"))
+
+        localStorage.setItem("bodyColor" , elColor[i].getAttribute("data-color"))
+
+    })
+
+}
 
 // Portfolio Section 
 
